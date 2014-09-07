5188f28697b05995648bac296289687d134e58e9
mailmesam <mailmesam@users.noreply.github.com>
2014-09-07 18:30:10 +0530

    Create t.sh

---
+++ t.sh
@@ -0,0 +1,25 @@
+sudo apt-get update
+sudo apt-get dist-upgrade
+
+sudo pm-hibernate
+
+sudo add-apt-repository ppa:tualatrix/ppa
+
+sudo apt-get update
+
+sudo apt-get install ubuntu-tweak
+
+#
+sudo apt-get remove indicator-cpufreq
+
+
+#compiz
+
+sudo apt-get install compizconfig-settings-manager compiz-plugins-extra -y
+
+
+
+
+sudo apt-get install ubuntu-restricted-extras
+
+sudo apt-get install vlc
